c7f3bc703c432d34a5f88162197ba8f0b112a8cd
siavava <amittaijoel@outlook.com>
2023-10-08 23:48:19 -0400

    clear up functions

--- src/main.rs
+++ src/main.rs
@@ -18,31 +18,75 @@ use rocket::{get, http::Status, serde::json::Json};
 use crate::database::Database;
 use crate::events::Event;
 
+///
+/// Hello world
+///
 #[get("/")]
 fn hello() -> Result<Json<String>, Status> {
   Ok(Json(String::from("Hello from rust and mongoDB")))
 }
 
+///
+/// Get all events from the database
+/// 
+/// ## Parameters
+/// 
+/// None
+/// 
+/// ## Returns
+/// 
+/// All events (JSON) wrapped in a Result
+/// 
 #[get("/events")]
 async fn get_events(db: &State<Database>) -> Result<Json<Vec<Event>>, Status> {
   let events = db.get_events().await.unwrap();
   Ok(Json(events))
 }
 
+///
+/// Get a single event from the database
+/// 
+/// ## Parameters
+/// 
+/// - `id` - The id of the event to get
+/// 
+/// ## Returns
+/// 
+/// The event (JSON) wrapped in a Result
+/// 
+#[get("/events/<id>")]
+async fn get_event(db: &State<Database>, id: String) -> Result<Json<Event>, Status> {
+  let event: Event = db.get_event(id).await.unwrap();
+  Ok(Json(event))
+}
+
+///
+/// Insert a new event into the database
+/// 
+/// ## Parameters
+/// 
+/// - `event` - The event to insert
+/// 
+/// ## Returns
+/// 
+/// The id of the inserted event wrapped in a Result
+///
 #[put("/events", data = "<event>")]
 async fn put_event(db: &State<Database>, event: Json<Event>) -> Result<Json<String>, Status> {
-  let id = db.insert_event(event.into_inner()).await.unwrap();
+  let id: String = db.insert_event(event.into_inner()).await.unwrap();
   Ok(Json(id))
 }
 
 #[launch]
 async fn rocket() -> _ {
-  let db = database::Database::init().await.unwrap();
+  
+  let db: Database = database::Database::init().await.unwrap();
   
   rocket::build()
     .manage(db)
     .mount("/", routes![hello])
     .mount("/", routes![get_events])
     .mount("/", routes![put_event])
+    .mount("/", routes![get_event])
 }
 
